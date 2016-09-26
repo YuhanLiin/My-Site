24521736d927b7c55974795480e1d2e6d57c8cc6
Yuhan Lin <linyuhan0315@hotmail.com>
2016-09-26 12:02:56 -0400

    Initial Commit

---
+++ js/front.js
@@ -0,0 +1,23 @@
+
+var descriptionID = $("#my-description");
+for (i in description){
+    descriptionID.append('<p>'+description[i]+'</p>');
+}
+
+var showcaseID = $("#showcase");
+for (i=0; i<3; i++){
+    showcaseID.append(projectHtml(gallery[i]));
+}
+
+///////////////////////////////////////////////////////////////////////
+//Animations
+animatedTitle = [];
+animatedTitle.push($("#animated-title > span:first-of-type"))
+animatedTitle.push($("#animated-title > span:nth-of-type(2)"))
+animatedTitle.push($("#animated-title > span:nth-of-type(3)"))
+
+$(document).ready(function(){
+    for (i in animatedTitle){
+        animatedTitle[i].delay(1500*i).animate({opacity:'1'}, 1500);
+    }
+});
---
+++ js/projects.js
@@ -0,0 +1,39 @@
+//Creates gallery
+var galleryID = $("#project-gallery");
+for (i in gallery){
+    galleryID.append(projectHtml(gallery[i]));
+}
+
+//Change porject-box colour when hovered over
+$(".project-box").hover(function(){
+    $(this).css("background-color","#e6e6e6");
+},
+function(){
+    $(this).css("background-color","#ffffff");
+});
+
+//Sliding animation for gallery elements
+var slideTime = 1000;
+var fadeTime = 300;
+$(".project-box").click(function(){
+    var descriptionBox = $(this).children(".project-description");
+    var descriptionP = descriptionBox.children('p');
+    //Slide box back in and stack the description under the box
+    if ($(this).data("slideout")){
+        descriptionP.animate({"opacity":"0"}, fadeTime).promise().done(function(){
+            descriptionP.delay(slideTime).css("z-index", "-1");
+        });
+        descriptionBox.delay(fadeTime).animate({"max-height":"0",
+                                            "height": "0"}, slideTime);
+        $(this).data("slideout", false);
+    }
+    //Slide box out and stack description on top of the box
+    else{
+        var height = descriptionP.height();
+        descriptionBox.animate({"max-height":height.toString(),
+                                "height": height.toString()}, slideTime);
+        descriptionP.delay(slideTime).animate({"opacity":"1"}, fadeTime);
+        $(this).data("slideout", true);
+        descriptionP.css("z-index", "10");
+    }
+});
